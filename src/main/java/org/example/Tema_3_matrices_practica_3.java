1f03c396c15ca6968446c29176155bcbba7ce9af
A703\Jacobo San Mauro <jacobitosmn@gmail.com>
2025-11-25 17:17:28 +0100

    no message

--- src/main/java/org/example/Tema_3_matrices_practica_3.java
+++ src/main/java/org/example/Tema_3_matrices_practica_3.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Tema_3_matrices_practica_3 {
@@ -9,7 +10,9 @@ public class Tema_3_matrices_practica_3 {
         Scanner read = new Scanner(System.in);
 
         boolean pepe = true;
+        String[] letras_dentro_fila = new String[4];
 
+        pepe:
         do {
 
             System.out.println("********* VALIDADOR DE COMPATIBILIDAD ZX SPECTRUM *********");
@@ -19,9 +22,68 @@ public class Tema_3_matrices_practica_3 {
             System.out.println("Alto:");
             int alto = read.nextInt();
 
+
             if ( ancho % 8 == 0 && alto % 8 == 0){
 
-                
+                    String[][] matriz  = new String[ancho][alto];
+
+                    System.out.println("Introduce una (linea a linea) los colores de tu imagen para cada pixel: ");
+
+                    for (int i = 0; i < matriz.length; i++) {
+
+                        String[] vector = read.next().split("");
+
+                        if ( vector[i].matches("a-oA-O") ){
+
+                            if (vector.length == matriz[i].length){
+
+                                for (int j = 0; j < matriz[i].length; j++) {
+
+                                    if (j == 7 || j == 15 || j ==23 || j == 31 || j == 39 || j == 47){
+
+                                        letras_dentro_fila = new String[4];
+
+                                    }
+
+                                   boolean color_dentro = Arrays.asList(letras_dentro_fila).contains(matriz[i][j]);
+
+                                    if (!color_dentro){
+
+                                        letras_dentro_fila[i] = matriz[i][j];
+
+                                    }
+
+                                    if ( !letras_dentro_fila[2].equals(""))
+
+                                    matriz[i][j] = vector[i];
+
+                                }
+
+                            } else {
+
+                                System.out.println("El ancho o el alto no son aptos! Vuelve a intentarlo");
+                                continue pepe;
+
+                            }
+
+                        } else {
+
+                            System.out.println("La letra introducida no esta entre : A y O");
+                            continue pepe;
+
+                        }
+
+                    }
+
+
+
+
+
+            pepe = false;
+
+            } else {
+
+                System.out.println("El ancho o el alto no son aptos! Vuelve a intentarlo");
 
             }
 
